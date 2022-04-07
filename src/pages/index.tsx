77b397d658bbbc9f54563c60c9ad95af5a385f0d
Avarile <Avarile@gmail.com>
2022-04-07 22:21:33 +1000

    routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import React from "react"
+import React from "react";
 
-export const Login = React.lazy(() => import("./Login"))
-export const MainEntrance = React.lazy(() => import("./MainEntrance"))
-export const DashboardMain = React.lazy(() => import("./DashBoardMain"))
-export const OrderManagment = React.lazy(() => import("./OrderManagment"))
-export const WareHousing = React.lazy(() => import("./WareHousing"))
-export const FallbackLoading = () => import("../components/FallbackLoading")
+export const Login = React.lazy(() => import("./Login"));
+export const MainEntrance = React.lazy(() => import("./MainEntrance"));
+export const DashboardMain = React.lazy(() => import("./DashBoardMain"));
+export const OrderManagment = React.lazy(() => import("./OrderManagment"));
+export const WareHousing = React.lazy(() => import("./WareHousing"));
+export const WareHousingInbound = React.lazy(
+  () => import("./warehouseing-inbound")
+);
+export const FallbackLoading = () => import("../components/FallbackLoading");
---
+++ src/pages/warehouseing-inbound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WarehousingInbound = () => {
+  return (
+    <>
+      <p>Hello from WarehousingInbound</p>
+    </>
+  );
+};
+
+export default WarehousingInbound;
