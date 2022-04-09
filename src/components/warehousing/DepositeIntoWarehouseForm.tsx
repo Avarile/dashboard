728a67ec0e06f46444ab6d98a1b58d92d277394b
qanglee <545540710@qq.com>
2022-04-09 19:28:39 +0800

    fix: 修复bug，表单刷新

--- src/components/warehousing/DepositeIntoWarehouseForm.tsx
+++ src/components/warehousing/DepositeIntoWarehouseForm.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useRef, useState } from "react"
-import "antd/dist/antd.css"
-import { Form, Input, InputNumber, Button, FormInstance, Select } from "antd"
-import Request from "@DATA/api.controller"
-import qs from "query-string"
-import { refineQueryString, isVoid, QueryStringType } from "@SRC/utils/utilFuncs"
-import useDebounce from "@SRC/Hooks/useDebounce"
-import { formatStrategyValues } from "rc-tree-select/lib/utils/strategyUtil"
+import React, { useEffect, useReducer, useRef, useState } from "react";
+import "antd/dist/antd.css";
+import { Form, Input, InputNumber, Button, FormInstance, Select } from "antd";
+import Request from "@DATA/api.controller";
+import qs from "query-string";
+import {
+  refineQueryString,
+  isVoid,
+  QueryStringType,
+} from "@SRC/utils/utilFuncs";
+import useDebounce from "@SRC/Hooks/useDebounce";
+import { formatStrategyValues } from "rc-tree-select/lib/utils/strategyUtil";
 
 const layout = {
   labelCol: {
@@ -14,7 +18,7 @@ const layout = {
   wrapperCol: {
     span: 20,
   },
-}
+};
 /* eslint-disable no-template-curly-in-string */
 
 const validateMessages = {
@@ -26,41 +30,43 @@ const validateMessages = {
   number: {
     range: "${label} must be between ${min} and ${max}",
   },
-}
+};
 /* eslint-enable no-template-curly-in-string */
 
 const WarehousingDepositeForm = () => {
+ 
   // useRef example usage as  refering an instance of a component
   // 1st step: create a ref
-  const ref = useRef<FormInstance<any> | null>()
+  const ref = useRef<FormInstance<any> | null>();
 
   const onFinish = (values: any) => {
-    console.log(values)
-    console.log(ref.current?.getFieldValue("product"))
-  }
-  const [products, setProducts] = useState<any>([])
+    console.log(values);
+    console.log(ref.current?.getFieldValue("product"));
+  };
+  const [products, setProducts] = useState<any>([]);
   const [searchParams, setSearchParams] = useState<QueryStringType>({
     name: "",
     sku: "",
-  })
+  });
 
-  const debouncedSearchParams = useDebounce(searchParams, 3000)
+  const debouncedSearchParams = useDebounce(searchParams, 3000);
 
-  const getProductData = () => {
-    Request.get(`http://localhost:3001/products?${qs.stringify(refineQueryString(searchParams))}`)
-      .then((response: any) => {
-        setProducts(response)
-        console.log(response)
-      })
-      .catch((error: any) => {
-        throw new Error(error)
-      })
-  }
-
-  useEffect(() => {
-    getProductData()
-  }, [setSearchParams, searchParams])
+  const getProductData = (queryData: { name?: string; sku?: string } = {}) => {
+    return Request.get(
+      `http://localhost:3001/products?${qs.stringify(
+        refineQueryString(queryData)
+      )}`
+    );
+  };
   
+  // 生命周期hook执行，切记不是事件执行，依赖为啥叫依赖而不是监听源头，不是事件驱动的。
+  useEffect(() => {
+    getProductData().then((response)=>{
+      setProducts(response)
+      console.log(response)
+    });
+  }, []);
+
   return (
     <Form
       initialValues={{}}
@@ -71,57 +77,74 @@ const WarehousingDepositeForm = () => {
       style={{ flex: 1 }} // flex: 1 的作用
       //ref need to receive a instance of a component using a function to pass it into the current state of the ref.
       ref={(formInstance: FormInstance<any> | null) => {
-        ref.current = formInstance
-      }}>
+        ref.current = formInstance;
+      }}
+    >
       <Form.Item label="Product Name" style={{ marginBottom: 0 }}>
-        <Form.Item name={["product", "productName"]} rules={[{ required: true }]} style={{ display: "inline-block", width: "calc(50% - 8px)", paddingRight: "5px" }}>
+        <Form.Item
+          name={["product", "productName"]}
+          rules={[{ required: true }]}
+          style={{
+            display: "inline-block",
+            width: "calc(50% - 8px)",
+            paddingRight: "5px",
+          }}
+        >
           <Input
-            // onChange={(event) => {
-            //   setSearchParams({
-            //     ...searchParams,
-            //     name: event.target.value,
-            //   })
-            // }}
-
             onChange={() => {
-              debugger
-              console.log(ref.current?.getFieldValue("product").productName)
+              console.log(ref.current?.getFieldValue("product").productName);
             }}
           />
         </Form.Item>
 
-        <Form.Item name={["product", "productSku"]} style={{ display: "inline-block", width: "calc(50% - 8px)" }}>
+        <Form.Item
+          name={["product", "productSku"]}
+          style={{ display: "inline-block", width: "calc(50% - 8px)" }}
+        >
           <Select
             placeholder="Select Product"
             onChange={() => {
-              Request.get(`http://localhost:3001/products?${qs.stringify(refineQueryString(searchParams))}`)
+              // 当select数据变化时，获得form表单的product的数据
+              let currentFormValue = ref.current?.getFieldValue("product");
+              let queryData = {
+                sku: currentFormValue.productSku,
+              };
+              // 根据选择项的数据，进行请求
+              getProductData(queryData)
                 .then((response: any) => {
-                  console.log(response)
-                  debugger
-                  let currentFormValue = ref.current?.getFieldValue("product")
+                  console.log(response);
+                  const {
+                    name: productName,
+                    currentInStock: productQuantityInstock,
+                    productSku: sku,
+                  } = response?.[0] || {};
+                  // 设置表单数据
+                  // setFieldsValue是底层包装了state，确保了表单刷新
+                  // 但是，setFieldsValue并不会引发当前组件的刷新
+                  // 表单是子组件
+                  // 子组件刷新不会引发父组件刷新
+                  // 当前组件是父组件，表单是子组件
+                  // setFieldsValue触发了表单的数显，即子组件的刷新，确保了表单项目显示正确
+                  // 当前组件不会刷新，也就不会出发useEffect
                   ref.current?.setFieldsValue({
                     product: {
                       ...currentFormValue,
-                      productName: response[0]?.name,
-                      productQuantityInstock: response[0]?.currentInStock,
+                      productName,
+                      productQuantityInstock,
                     },
-                  })
-                  setSearchParams({
-                    ...searchParams,
-                    sku: ref.current?.getFieldValue("product").productSku,
-                  })
+                  });
                 })
                 .catch((error: any) => {
-                  throw new Error(error)
-                })
-            }}>
+                  throw new Error(error);
+                });
+            }}
+          >
             {products.map((product: any) => {
-              debugger
               return (
                 <Select.Option value={product.sku} key={product.id}>
                   {product.sku}
                 </Select.Option>
-              )
+              );
             })}
           </Select>
         </Form.Item>
@@ -134,7 +157,8 @@ const WarehousingDepositeForm = () => {
             required: true,
             message: "must provide products SKU",
           },
-        ]}>
+        ]}
+      >
         <Input />
       </Form.Item>
       <Form.Item
@@ -145,7 +169,8 @@ const WarehousingDepositeForm = () => {
             type: "number",
             message: "Quantity has to be number",
           },
-        ]}>
+        ]}
+      >
         <InputNumber />
       </Form.Item>
       <Form.Item
@@ -157,29 +182,36 @@ const WarehousingDepositeForm = () => {
             min: 0,
             max: 99999,
           },
-        ]}>
+        ]}
+      >
         <InputNumber />
       </Form.Item>
       <Form.Item name={["product", "productDescription"]} label="Description">
         <Input.TextArea style={{ minHeight: "20rem", maxHeight: "25rem" }} />
       </Form.Item>
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit" block style={{ marginBottom: "1rem" }}>
+        <Button
+          type="primary"
+          htmlType="submit"
+          block
+          style={{ marginBottom: "1rem" }}
+        >
           Submit
         </Button>
         <Button
           onClick={() => {
-            ref.current?.resetFields()
+            ref.current?.resetFields();
             setSearchParams({
               name: "",
               sku: "",
-            })
+            });
           }}
-          block>
+          block
+        >
           Reset Form
         </Button>
       </Form.Item>
     </Form>
-  )
-}
-export default WarehousingDepositeForm
+  );
+};
+export default WarehousingDepositeForm;
